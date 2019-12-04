2fc756aa3bc7784566d22ef2830cc7e17db90ac8
Christian Sarnataro <christian.sarnataro@vimnmix.com>
2019-12-04 10:51:54 +0100

    Modified how the build id is retrieved.

--- next.config.js
+++ next.config.js
@@ -1,5 +1,17 @@
 require('dotenv').config()
-const nextBuildId = require('next-build-id')
+// const nextBuildId = require('next-build-id')
+// next.config.js
+const fs = require('fs')
+const cp = require('child_process')
+
+const generateBuildId = () => {
+  if (!fs.existsSync('git-commit-hash')) {
+    cp.execSync('git rev-parse HEAD > git-commit-hash')
+  }
+  const gitCommitHash = fs.readFileSync('git-commit-hash', { encoding: 'utf-8' })
+
+  return gitCommitHash.substr(0, 8)
+}
 
 module.exports = {
   env: {
@@ -7,9 +19,9 @@ module.exports = {
     // and make it available at Build Time
     EMAIL: process.env.EMAIL,
     BUILD_DIR: __dirname,
-    BUILD_ID: nextBuildId.sync().substr(0, 8)
+    BUILD_ID: generateBuildId()
   },
-  generateBuildId: () => nextBuildId({ dir: __dirname }),
+  generateBuildId: () => generateBuildId(),
   webpack: (config) => {
     config.module.rules.push(
       {
